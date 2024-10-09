a9359b312510bfe4196a9c2566bbf902bf531f03
Dintaifung77 <zbelgum@ucsd.edu>
2024-10-09 16:23:22 +0800

    6 alternate loop 3 times

---
+++ 6_repeat_three_times2.py
@@ -0,0 +1,25 @@
+#
+# Zach
+# Repeat the task three times
+#
+
+max_lap = 3
+curr_lap = 1
+
+while (curr_lap <= max_lap):
+    # 1. Input
+    n1 = input('Number 1: ')
+    n2 = input('Number 2: ')
+
+    # 2. Process
+    if int(n1) > int(2):
+        bigger = int(n1)
+    elif int(n1) < int(n2):
+        bigger = int(n2)
+    else:
+        bigger = 'Same'
+
+    # 3. Output
+    print(f'Bigger : {bigger}')
+
+    curr_lap += 1
