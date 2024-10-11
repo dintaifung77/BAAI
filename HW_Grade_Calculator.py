3beadb0b4dcc2996290da5a2581cb22f828aeb28
dintaifung77 <zbelgum@ucsd.edu>
2024-10-11 12:45:47 +0800

    added line space

--- HW_Grade_Calculator.py
+++ HW_Grade_Calculator.py
@@ -5,17 +5,19 @@
 
 # 1. Input
 s = int(input('How many subjects do you have? '))
+print("\n")
 sum = 0
 count = 1 # count is currently equal to 1, but after the loop has run, it will increase to 2, 3, 4, and so on
 
 while (count <= s):
     s1 = input(f'Enter the name of subject {count}: ')
     n1 = int(input(f'Enter the grade for {s1}: '))
+    print("\n")
     sum = sum + n1
     count += 1     # count= count + 1
 
 # 2. Process
-avg = sum / s
+avg = int(sum / s)
 
 # 3. Output
 print (f'Your average grade is: {avg}')
