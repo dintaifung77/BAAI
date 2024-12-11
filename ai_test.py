ce1111d7731fc3428f82b465f99a89ab8e176974
dintaifung77 <zbelgum@ucsd.edu>
2024-12-11 15:16:18 +0800

    xzfkjdslkf

---
+++ ai_test.py
@@ -0,0 +1,12 @@
+# This script adds two integers together
+
+def main():
+    num1 = int(input("Please enter the first integer: "))
+    num2 = int(input("Please enter the second integer: "))
+
+    sum_of_nums = num1 + num2
+
+    print(f"The sum of {num1} and {num2} is: {sum_of_nums}")
+
+if __name__ == "__main__":
+    main()
